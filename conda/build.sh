52508f5589ad4d948d1d5bb0b7e5a591ce7439dc
Burkhard Ritter <burkhard@seite9.de>
2014-02-18 13:21:58 -0700

    Version bump. Minor changes to conda recipe.

--- conda/build.sh
+++ conda/build.sh
@@ -12,7 +12,6 @@ mkdir Release
 cd Release
 cmake \
     -DCMAKE_BUILD_TYPE=Release \
-    -DBASIS_NUMBER_OF_BITS=32 \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_INSTALL_RPATH=$LD_RUN_PATH \
     -DPYTHON_INCLUDE_PATH:PATH=$PREFIX/include/python${PY_VER} \
